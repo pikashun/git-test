d2f5151ae0c126c350f36b96ed0044f5fa5f54f9
pws <952704496@qq.com>
2021-05-27 12:22:43 +0800

    远程仓库

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-27 12:20:58
+ * @LastEditTime: 2021-05-27 12:21:49
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \git-test\index.js
+ */
+alert("first git project")
